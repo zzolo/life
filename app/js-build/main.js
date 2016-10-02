687591b3f1a72c580f33569bffd465722a487983
Alan Palazzolo <github@zzolo.org>
2016-10-01 23:04:49 -0500

    Fixing some things.

--- app/js-build/main.js
+++ app/js-build/main.js
@@ -4,5 +4,30 @@ var _react = require('react');
 
 var _react2 = _interopRequireDefault(_react);
 
+var _reactDom = require('react-dom');
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-//# sourceMappingURL=main.js.map
+
+/**
+ * Main JS file.
+ */
+
+(0, _reactDom.render)(_react2.default.createElement(
+  'div',
+  { className: 'welcome' },
+  _react2.default.createElement(
+    'div',
+    { className: 'welcome-box' },
+    _react2.default.createElement(
+      'h1',
+      null,
+      'Life'
+    ),
+    _react2.default.createElement(
+      'p',
+      null,
+      _react2.default.createElement('span', { className: 'loading' })
+    )
+  )
+), document.querySelector('#life'));
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIi4uL2pzL21haW4uanMiXSwibmFtZXMiOlsiZG9jdW1lbnQiLCJxdWVyeVNlbGVjdG9yIl0sIm1hcHBpbmdzIjoiOztBQUlBOzs7O0FBQ0E7Ozs7QUFMQTs7OztBQU9BLHNCQUNFO0FBQUE7QUFBQSxJQUFLLFdBQU0sU0FBWDtBQUNFO0FBQUE7QUFBQSxNQUFLLFdBQU0sYUFBWDtBQUNFO0FBQUE7QUFBQTtBQUFBO0FBQUEsS0FERjtBQUVFO0FBQUE7QUFBQTtBQUFHLDhDQUFNLFdBQU0sU0FBWjtBQUFIO0FBRkY7QUFERixDQURGLEVBT0VBLFNBQVNDLGFBQVQsQ0FBdUIsT0FBdkIsQ0FQRiIsImZpbGUiOiJtYWluLmpzIiwic291cmNlc0NvbnRlbnQiOlsiLyoqXG4gKiBNYWluIEpTIGZpbGUuXG4gKi9cblxuaW1wb3J0IFJlYWN0IGZyb20gJ3JlYWN0JztcbmltcG9ydCB7IHJlbmRlciB9IGZyb20gJ3JlYWN0LWRvbSc7XG5cbnJlbmRlcihcbiAgPGRpdiBjbGFzcz1cIndlbGNvbWVcIj5cbiAgICA8ZGl2IGNsYXNzPVwid2VsY29tZS1ib3hcIj5cbiAgICAgIDxoMT5MaWZlPC9oMT5cbiAgICAgIDxwPjxzcGFuIGNsYXNzPVwibG9hZGluZ1wiPjwvc3Bhbj48L3A+XG4gICAgPC9kaXY+XG4gIDwvZGl2PixcbiAgZG9jdW1lbnQucXVlcnlTZWxlY3RvcignI2xpZmUnKVxuKTtcbiJdfQ==
--- app/js/main.js
+++ app/js/main.js
@@ -3,3 +3,14 @@
  */
 
 import React from 'react';
+import { render } from 'react-dom';
+
+render(
+  <div class="welcome">
+    <div class="welcome-box">
+      <h1>Life</h1>
+      <p><span class="loading"></span></p>
+    </div>
+  </div>,
+  document.querySelector('#life')
+);
